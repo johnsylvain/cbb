172862a6f06676d9ce8c3bfa6d00a94c3c79272b
John Sylvain <john.n.sylvain@gmail.com>
2019-03-24 21:12:19 -0500

    add filtering by conference

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 #!/usr/bin/env node
 'use strict';
 
-const fetch = require('node-fetch');
-const clc = require('cli-color');
-const Table = require('cli-table');
-const { format } = require('date-fns');
+const fetch = require('node-fetch')
+const clc = require('cli-color')
+const Table = require('cli-table')
+const { format } = require('date-fns')
+const meow = require('meow')
 
-(async () => {
+const cli = meow(`
+	Usage
+	  $ ncaa
+
+	Options
+	  --conference, -c  filter by converence
+
+	Examples
+	  $ ncaa --conference big-ten
+`, {
+    flags: {
+      conference: {
+        type: 'string',
+        alias: 'c'
+      }
+    }
+  });
+
+const ncaa = async (flags) => {
   const date = format(new Date(), 'YYYY/MM/DD')
   const table = new Table({
-    head: [clc.bold.white('Teams'), clc.bold.white('Score')],
+    head: ['Teams', 'Score'].map(text => clc.bold.white(text)),
     colWidths: [20, 20]
   })
   const res = await fetch(`https://data.ncaa.com/casablanca/scoreboard/basketball-men/d1/${date}/scoreboard.json`)
@@ -21,6 +40,13 @@ const { format } = require('date-fns');
   }
 
   games
+    .filter(({ game }) =>
+      !flags.conference ||
+      game.home.conferenceNames.conferenceName.toLowerCase() === flags.conference ||
+      game.home.conferenceNames.conferenceSeo.toLowerCase() === flags.conference ||
+      game.away.conferenceNames.conferenceName.toLowerCase() === flags.conference ||
+      game.away.conferenceNames.conferenceSeo.toLowerCase() === flags.conference
+    )
     .map(({ game }) => {
       const {
         away,
@@ -40,15 +66,17 @@ const { format } = require('date-fns');
       const details = gameState === 'pre'
         ? `${startTime} ${network}`
         : `${awayTeam(away.score)}   ${currentPeriod}\n${homeTeam(home.score)}   ${
-          gameState === 'live' ? contestClock : ''
+        gameState === 'live' ? contestClock : ''
         }`
       const ranking = rank => rank ? `(${rank})` : '';
       table.push([`${
-          awayTeam(`${away.names.short} ${ranking(away.seed || away.rank)}`)
+        awayTeam(`${away.names.short} ${ranking(away.seed || away.rank)}`)
         }\n${
-          homeTeam(`${home.names.short} ${ranking(home.seed || home.rank)}`)
+        homeTeam(`${home.names.short} ${ranking(home.seed || home.rank)}`)
         }`, details])
     })
 
   console.log(table.toString())
-})()
+}
+
+ncaa(cli.flags)
